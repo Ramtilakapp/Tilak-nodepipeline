5d48eb6adbe967c825a8bdf14f8a454bf1cff6a5
Ramtilakapp <ramtilak.yadav@appinventiv.com>
2024-09-19 04:18:42 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
-forever start /var/nodeapp/src/index.js
+
+# Stop any running container
+if [ "$(docker ps -q -f name=tilak-3-node-app)" ]; then
+    docker stop tilak-3-node-app
+    docker rm tilak-3-node-app
+fi
+
+# Pull the latest image from ECR
+$(aws ecr get-login --no-include-email --region eu-north-1)
+docker pull 038462757595.dkr.ecr.ap-south-1.amazonaws.com/tilak-3-node-app:latest
+
+# Run the new container
+docker run -d -p 3000:3000 --name tilak-3-node-app 038462757595.dkr.ecr.ap-south-1.amazonaws.com/tilak-3-node-app:latest
+
+
